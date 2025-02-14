a283c835ffd7dd90c49ed6f77c29886642b2c555
Martin-DMC <the.chiperfil@gmail.com>
2025-02-14 18:00:05 +0000

    5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - imprime un mensaje en pantalla
+ * Description: escribe en pantalla usando el comando printf
+ * Return: retorna 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
