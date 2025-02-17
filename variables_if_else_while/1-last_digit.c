1ec28c582eaeb235b3e226647908255407ee4aa7
Martin-DMC <the.chiperfil@gmail.com>
2025-02-17 13:00:29 +0000

    1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,11 +12,12 @@
 int main(void)
 {
 	int n;
+	int last_d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_d = n % 10;
+	last_d = n % 10;
 	if (last_d == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, last_d);
