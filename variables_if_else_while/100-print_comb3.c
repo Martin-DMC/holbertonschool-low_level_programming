b90915758c4f53711612b5c88d59b2c97b55907c
Martin-DMC <the.chiperfil@gmail.com>
2025-02-17 21:11:55 +0000

    100-print_comb3

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+/**
+ * main - imprime conbinaciones
+ * Description: imprime combinaciones de numeros simples
+ * Return: retorna 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (!(i == 9 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+
+			}	
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
