52725e5ab15aa199432228c6815bc063a1eb1457
Martin-DMC <the.chiperfil@gmail.com>
2025-02-17 15:46:16 +0000

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * main - imprime por pantalla
+ * Description: imprime una cadena de numeros por pantalla
+ * Return: retorna 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	return (0);
+}
