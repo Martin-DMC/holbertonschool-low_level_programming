ac6a00aa84479f831d38d888d39555f9f37965e8
Martin-DMC <the.chiperfil@gmail.com>
2025-02-17 21:20:20 +0000

    100-print_comb3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,14 @@ int main(void)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
 
-			if (!(i == 9 && j == 9))
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
