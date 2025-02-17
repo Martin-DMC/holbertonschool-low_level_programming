e35826f91ef7d58b20f80990432ee50568768b6b
Martin-DMC <the.chiperfil@gmail.com>
2025-02-17 16:25:37 +0000

    7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/**
+ * main - imprime el abecedario
+ * Description: imprime todo el abecedario con putchar
+ * Return: retorna 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	char str[] = "zyxwvutsrqponmlkjihgfedcba";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
