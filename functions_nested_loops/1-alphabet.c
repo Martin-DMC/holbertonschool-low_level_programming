91e03449cf49c0a7296953f71d3789348308d2c2
Martin-DMC <the.chiperfil@gmail.com>
2025-02-18 15:11:59 +0000

    1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,21 @@
-uu
+/**
+ * print_alphabet - imprime mensaje
+ * Description: imprime el abecedario por pantalla
+ * Return: retorna 0
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char str[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
