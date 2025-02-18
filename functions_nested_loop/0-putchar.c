2a6aff58f7d9c38ae71230a133217eeba4be4972
Martin-DMC <the.chiperfil@gmail.com>
2025-02-18 13:46:08 +0000

    0-putchar

---
+++ functions_nested_loop/0-putchar.c
@@ -0,0 +1,21 @@
+/**
+ * main - imprime mensaje
+ * Description: imprime menaje por pantalla
+ * Return: retorna 0
+ */
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
