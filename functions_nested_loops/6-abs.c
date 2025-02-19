a26725327bd0facf5ac6012025b79e1e7cee6bad
Martin-DMC <the.chiperfil@gmail.com>
2025-02-19 14:59:55 +0000

    6-abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - num absouto
+ * @int: numero
+ *
+ * Description: dice el numero absoluto
+ * Return: retorna 0
+ */
+
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+int _abs(int _)
+{
+	_ = abs(_);
+	return (_);
+}
