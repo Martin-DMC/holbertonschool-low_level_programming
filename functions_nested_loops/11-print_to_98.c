3422e39b26c30e2ecbc51a9a9cb6a7f4484ec464
Martin-DMC <the.chiperfil@gmail.com>
2025-02-20 00:13:53 +0000

    11-print_to_98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+/**
+ * print_to_98 - muestra secuencia
+ * @n: numero 
+ *
+ * Description: muestra secuencia hasta el 98
+ * Return: retorna 0
+ */
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			n--;
+			if (n >= 98)
+			{
+				printf(", ");
+			}
+			else
+			{
+				printf("\n");
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			n++;
+			if (n <= 98)
+			{
+				printf(", ");
+			}
+			else
+			{
+				printf("\n");
+			}
+		}
+	}
+
+}
