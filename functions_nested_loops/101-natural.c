9de759a4ed94103d429723ea8cbc296e851f93eb
Martin-DMC <the.chiperfil@gmail.com>
2025-02-21 17:29:24 +0000

    101-natural

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -11,15 +11,26 @@ void sumar_multiplos(int num1, int num2)
 {
 	int i, sum;
 
-	for (i = 2; i < 1000 || sum <= 1024; i++)
+	for (i = 2; i < 1000 && sum < 1025; i++)
 	{
+
 		if (i % num1 == 0 || i % num2 == 0)
 		{
-			printf("%d, ", i);
+			if ((sum + i) > 1024)
+			{
+				printf("\n");
+			}
+			else if ((1024 - (sum + i)) < i)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
 			sum = sum + i;
 		}
 	}
-	printf("\n");
 }
 /**
  * main - punto de entrada del programa
