02b8bf344b22050d17e6d54099d61886bd0ef54c
Martin-DMC <the.chiperfil@gmail.com>
2025-02-22 16:09:31 +0000

    102-fibonacci

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,45 @@
+/**
+ * fibonacci - imprime numeros
+ * @num1: first num
+ * @num2: second num
+ *
+ * Description: imprime los primeros 50 num fibonacci
+ */
+#include <stdio.h>
+
+void fibonacci(long num1, long num2)
+{
+	long r;
+
+	printf("%ld, %ld,", num1, num2);
+
+	for (int i = 0; i < 48; i++)
+	{
+		r = num1 + num2;
+		if (i == 47)
+		{
+			printf(" %ld\n", r);
+		}
+		else
+		{
+			printf(" %ld,", r);
+		}
+		num1 = num2;
+		num2 = r;
+
+	}
+}
+/**
+ * main - punto de entrada del programa
+ *
+ * Description: llama la a la funcion anterior
+ *
+ * Return: retorna 0
+ */
+
+int main(void)
+{
+	fibonacci(1, 2);
+
+	return (0);
+}
