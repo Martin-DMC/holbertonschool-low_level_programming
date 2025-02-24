7aae42fa5679e6ac63f08821fb8787e2540be4b3
Martin-DMC <the.chiperfil@gmail.com>
2025-02-24 13:54:58 +0000

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - multiplicacion
+ * @a: numero
+ * @b: numero
+ *
+ * Description: multiplica 2 enteros
+ * Return: retorna resultado
+ */
+
+int mul(int a, int b)
+{
+	int r = a * b;
+
+	return (r);
+}
