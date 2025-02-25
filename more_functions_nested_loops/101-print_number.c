0ce00cf646199281b42bcbe0cabcd6ce164d3533
Martin-DMC <the.chiperfil@gmail.com>
2025-02-25 15:11:00 +0000

    101-print_number

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+/**
+ * print_number - imprime numeros
+ * @n: numero
+ *
+ * Description: imprime numeros por pantalla
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar('0' + (n % 10));
+}
