17fcac0acf1551276bc25c7e5148f3cc48c3000f
Martin-DMC <the.chiperfil@gmail.com>
2025-02-25 17:45:15 +0000

    2-strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - cuenta caracteres
+ * @s: cadena
+ *
+ * Description: cuenta una cadena de caracteres
+ *
+ * Return: cantidad de caracteres
+ */
+#include <string.h>
+
+int _strlen(char *s)
+{
+	int r = strlen(s);
+
+	return (r);
+}
