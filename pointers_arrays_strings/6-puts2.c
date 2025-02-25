f6bbbda66554f9c532ee980ad71c4faa3b8e39aa
Martin-DMC <the.chiperfil@gmail.com>
2025-02-25 22:47:11 +0000

    6-puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+/**
+ * puts2 - imprime mensaje
+ * @str: puntero
+ *
+ * Description: imprime los otros caracteres
+ */
+#include "main.h"
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
