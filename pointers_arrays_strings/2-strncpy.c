b9456ec239d7468fe062b2013e9889a9374c1e77
Martin-DMC <the.chiperfil@gmail.com>
2025-02-27 16:26:26 +0000

    2-strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,35 @@
+/**
+ * _strncpy - copia texto
+ * @dest: destino
+ * @src: original
+ * @n: cantidad
+ *
+ * Description: copia dos textos diferentes
+ *
+ * Return: retorna un puntero
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *r = dest;
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		if (src[j] == '\0')
+		{
+			while (i < n)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
+	}
+
+	return (r);
+}
