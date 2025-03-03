2aeaa123813ec3bd78b9cdd031ea9209ad46d705
Martin-DMC <the.chiperfil@gmail.com>
2025-03-03 17:16:37 +0000

    5-strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+/**
+ * _strstr - devuelve puntero a la 1er coincidencia
+ * @haystack: cadena
+ * @needle: subcadena
+ *
+ * Return: retorna puntero
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j;
+
+	while (needle[i])
+	{
+		j = 0;
+		while (haystack[j])
+		{
+			if (needle[i] == haystack[j])
+			{
+				return (&needle[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return ('\0');
+}
