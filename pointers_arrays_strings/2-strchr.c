2aadf46bf7db3e4190d33db9490e74000c565463
Martin-DMC <the.chiperfil@gmail.com>
2025-03-03 15:11:32 +0000

    2-strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+/**
+ * _strchr - elimina caracteres
+ * @s: puntero
+ * @c: caracter
+ *
+ * Return: retorna null o puntero
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return ('\0');
+}
