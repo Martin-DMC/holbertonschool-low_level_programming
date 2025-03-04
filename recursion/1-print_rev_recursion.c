2efda812af209c95944af098601060e8f8d44c38
Martin.DMC <the.chiperfil@gmail.com>
2025-03-04 13:03:58 +0000

    1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _print_rev_recursion - imprime texto inversos
+ * @s: puntero
+ */
+#include "main.h"
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
