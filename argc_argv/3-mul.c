284061f5ae0e1759af7d9bba348a13f0e43c5de7
Martin-DMC <the.chiperfil@gmail.com>
2025-03-05 23:25:06 +0000

    3-mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+/**
+ * main - multiplica dos argumentos
+ * @argc: cantidad
+ * @argv: array
+ *
+ * Return: 0, error 1
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int resultado;
+
+	if (argc != 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
+	a = atoi(argv[1]);
+        b = atoi(argv[2]);
+        resultado = a * b;
+	printf("%d\n", resultado);
+	return (0);
+}
