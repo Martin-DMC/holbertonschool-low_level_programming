588438d7da48e5ee461c44d1c2211c11ccee48e1
Martin-DMC <the.chiperfil@gmail.com>
2025-03-05 13:27:23 +0000

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,23 @@
+/**
+ * main1 - muestra cantidad de argumentos
+ * @argc: cantidad
+ * @argv: argumentos
+ *
+ * Return: retorna 0
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	}
+	printf("%d\n", i - 1);
+	if (argv != 0)
+	{
+		return (0);
+	}
+	return (0);
+}
