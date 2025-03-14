2752e55b58a5e15b81e49a49074065a7ea224c40
Martin-DMC <the.chiperfil@gmail.com>
2025-03-14 20:42:23 +0000

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,33 @@
 #include "dog.h"
-#include <string.h>
 #include <stdlib.h>
+/**
+ * _strdup - copia cadena
+ * @str: cadena original
+ *
+ * Return: puntero a copia o NULL
+ */
+char *_strdup(char *str)
+{
+	unsigned int len = 0, i;
+	char *copia;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	copia = (char *)malloc((len + 1) * sizeof(char));
+	if (copia == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		copia[i] = str[i];
+	}
+	return (copia);
+}
 /**
  * new_dog - crea un nuevo perro
  * @name: puntero a nombre
@@ -17,14 +44,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d->name = strdup(name);
+	d->name = _strdup(name);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
 
-	d->owner = strdup(owner);
+	d->owner = _strdup(owner);
 	if (d->owner == NULL)
 	{
 		free(d->name);
