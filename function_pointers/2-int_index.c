dd76736a6e4b1e46ec4c47027d794011c248b4fd
Martin-DMC <the.chiperfil@gmail.com>
2025-03-17 19:21:57 +0000

    2-int_index.c 1

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * int_index - returns the position of the first match
  * @array: numbers array
@@ -12,6 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0, a = 0;
 
 	if (size <= 0)
+		return (-1);
+
+	if (array == NULL)
+		return (-1);
+
+	if (cmp == NULL)
 		return (-1);
 
 	while (i < size)
