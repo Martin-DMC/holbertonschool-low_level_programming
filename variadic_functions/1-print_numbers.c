c839ccc4bee31c8a5640338f946acfdf56820443
Martin-DMC <the.chiperfil@gmail.com>
2025-03-19 15:12:47 +0000

    2do ejercicio

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - imprime los numeros variables que le pasemos
+ * @separator: =! NULL se usa para separar los valores
+ * @n: cantidad de valores
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int valor;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+		{
+			valor = va_arg(args, int);
+			if (i == 0)
+				printf("%d%s", valor, separator);
+			else if (i == (n - 1))
+				printf(" %d\n", valor);
+			else
+				printf(" %d%s", valor, separator);
+		}
+		else
+		{
+			valor = va_arg(args, int);
+			if (i == 0)
+				printf("%d", valor);
+			else if (i == (n - 1))
+				printf(" %d", valor);
+			else
+				printf(" %d\n", valor);
+		}
+	}
+
+}
