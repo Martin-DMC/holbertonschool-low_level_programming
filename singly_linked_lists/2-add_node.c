642cc032ba68bdd481250e69910f3a48fdcd392c
Martin-DMC <the.chiperfil@gmail.com>
2025-04-02 14:10:21 +0000

    complaciendo a betty style y doc

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,14 +1,22 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ * add_node - agrega nuevos nodos al principio de la lista
+ * @head: puntero con la direccion de cabeza de lista
+ * @str: contenido del nodo
+ *
+ * Return: puntero al nuevo nodo o NULL
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 	int len, i;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 	else
 	{
 		len = 0;
