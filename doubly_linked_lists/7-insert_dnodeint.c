0ccac294b37e8a4e80706c9f2301a627f1a834c1
Martin-DMC <the.chiperfil@gmail.com>
2025-04-03 16:21:42 +0000

    agregar nodos al medio de la lista

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,12 +1,88 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
+ * cont_nodo - contador de nodos auxiliar
+ * @h: puntero a la cabeza de la lista
+ *
+ * Return: cantidad de nodos;
+ */
+unsigned int cont_nodo(dlistint_t **h)
+{
+	dlistint_t *tmp = *h;
+	unsigned int n = 0;
+
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		n++;
+	}
+	return (n);
+}
+/**
+ * caso_igual_uno - maneja el caso en que idx es == 1
+ * @h: puntero a la cabeza
+ * @new: puntero al nuevo nodo
+ * @next: puntero a la variable next
+ *
+ * Return: puntero a new modificado
+ */
+dlistint_t *caso_igual_uno(dlistint_t **h, dlistint_t *new, dlistint_t *next)
+	{
+		next = (*h)->next;
+		new->next = next;
+		new->prev = *h;
+		(*h)->next = new;
+		if ((*h)->next != NULL)
+			(new->next)->prev = new;
+		return (new);
+	}
+/**
+ * add_nodo - adiere nodos en medio de la lista
+ * @new: puntero al nodo new
+ * @next: puntero auxiliar
+ * @h: cabeza de lista
+ * @idx: indice necesario
+ *
+ * Return: puntero a new modificado
+ */
+dlistint_t *add_nodo(
+		dlistint_t *new,
+		dlistint_t *next,
+		dlistint_t **h,
+		unsigned int idx
+		)
+{
+	dlistint_t *tmp = *h;
+	unsigned int cant_nodo = 0;
+
+	while (tmp != NULL)
+	{
+		if (cant_nodo == (idx - 1))
+		{
+			next = tmp->next;
+			new->next = next;
+			new->prev = tmp;
+			tmp->next = new;
+			(new->next)->prev = new;
+		}
+		tmp = tmp->next;
+		cant_nodo++;
+	}
+	return (new);
+}
+/**
+ * insert_dnodeint_at_index - agrega un nodo e una posicion especifica
+ * @h: puntero a la cabeza de la lista
+ * @idx: posicion deseada
+ * @n: contenido del nodo
+ *
+ * Return: puntero al nuevo nodo o NULL
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp = *h;
-	unsigned int cont_nodo = 0;
+	dlistint_t *new, *next = NULL;
+	unsigned int cant_nodo = 0;
 
 	if (*h == NULL)
 		return (NULL);
@@ -14,42 +90,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-		cont_nodo++;
-	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		new = add_dnodeint(h, n);
 	}
-	else if (idx == cont_nodo)
+	cant_nodo = cont_nodo(h);
+	if (idx == cant_nodo)
 	{
-		add_dnodeint_end(h, n);
+		new = add_dnodeint_end(h, n);
 	}
-	else if (idx > cont_nodo)
+	else if (idx > cant_nodo)
 	{
 		return (NULL);
 	}
+	else if (idx == 1)
+	{
+		new = caso_igual_uno(h, new, next);
+	}
 	else
 	{
-		tmp = *h;
-		while (tmp != NULL)
-		{
-			if (idx == 1)
-			{
-				new->prev = tmp;
-				tmp->next = new;
-			}
-			if (idx == 0)
-			{
-				new->next = tmp;
-				tmp->prev = new;
-			}
-			tmp = tmp->next;
-			idx--;
-		}
+		new = add_nodo(new, next, h, idx);
 	}
-
 	return (new);
 }
